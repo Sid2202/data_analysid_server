01d92cda8f58c57d9ec5e1726ef8e96b32fc0e59
Sidhanti Patil <sidpatil2209@gmail.com>
2022-08-08 12:29:50 +0530

    first commit

---
+++ connect.js
@@ -0,0 +1,13 @@
+var mysql2 = require('mysql');
+
+module.exports = con = mysql2.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1Sidhanti$",
+  database: "testdb",
+  dialect: "mysql"
+});
+
+
+
+ 
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+var corsOptions = {
+  origin: "http://localhost:3001"
+};
+app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Analysiss" });
+});
+// set port, listen for requests
+// require("./app/routes/tutorial.routes")(app);
+// require("./app/routes/customer.routes")(app);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+// GET, POST, DELETE, PUT
+// app.get('/tuts', (req,res)=>{
+//   res.send('list of all tuts')
+// })
+
+
+// app.post('/api/add', (req,res)=>{
+//   console.log(req.body)
+//   res.send('add tutorial')
+// })
+
+
+// app.post('/api/cust',(req,res)=>{
+//   console.log(req.body)
+//   res.send('customer id:', res.cid)
+// })
+const element={}
+const db = require('./connect')
+const {parse, stringify} = require('flatted');
+const a = db.query("SELECT * FROM customers WHERE age>25", function (err, result) {
+    if (err) throw err;
+    console.log(stringify(result[0]));
+  });
+
+  app.get("/a", (req, res) => {
+    res.send(stringify(a[0]));
+  });
